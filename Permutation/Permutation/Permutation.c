576f8330b8de9d617f95807fee7f6e646776e661
psh3253 <psh3253@naver.com>
2020-09-15 19:02:26 +0900

    Add Permutation

---
+++ Permutation/Permutation/Permutation.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define SWAP(x, y, t)	((t) = (x), (x) = (y), (y) = (t))
+
+void perm(char* list, int i, int n)
+{
+	int j, temp;
+	if (i == n)
+	{
+		for (j = 0; j <= n; j++)
+			printf("%c", list[j]);
+		printf("	");
+	}
+	else
+	{
+		for (j = i; j <= n; j++)
+		{
+			SWAP(list[i], list[j], temp);
+			perm(list, i + 1, n);
+			SWAP(list[i], list[j], temp);
+		}
+	}
+}
+
+int main(void)
+{
+	char list[3] = { 'a', 'b', 'c' };
+	perm(list, 0, 2);
+	return 0;
+}
